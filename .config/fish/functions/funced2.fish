b93bca9e1a13cb9f09783dcdfb0f49fc59e44ec5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-08-20 16:40:59 -0500

    updated FishNotes

--- .config/fish/functions/funced2.fish
+++
@@ -1,8 +0,0 @@
-function funced2 -a name -d better way to edit functions
-  if test -z name
-    echo function name is required
-    return 1
-  end
-  set fnpath $fish_function_path[1]
-  funced $name; and funcsave $name; and source $fnpath/$name'.fish'
-end
