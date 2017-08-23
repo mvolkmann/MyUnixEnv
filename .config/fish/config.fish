928bf7e14099681932866717094570e943a79e2e
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-08-22 20:17:00 -0500

    added logo.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,11 +25,11 @@ set -U fish_greeting ''
 #---------------------------------------------------------------------------
 
 # For git
-abbr br git branch
-abbr ci git commit -av
-abbr git checkout
-abbr git log
-abbr git status
+abbr --add br git branch
+abbr --add ci git commit -av
+abbr --add co git checkout
+abbr --add log git log
+abbr --add status git status
 
 # For npm
 abbr --add nr npm run
