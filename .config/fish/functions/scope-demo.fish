b8f8275b3a70af03755eaf3455dafc08441e20b2
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-09-25 20:13:13 -0500

    updated fish.html

--- .config/fish/functions/scope-demo.fish
+++ .config/fish/functions/scope-demo.fish
@@ -4,7 +4,10 @@ function scope-demo
   begin
     set -l foo 3
     echo in block, foo = $foo
+    set bar 4
   end
   echo in function, foo = $foo
+  echo in function, bar = $bar
 end
 echo outside function, foo = $foo
+echo outside function, bar = $bar
