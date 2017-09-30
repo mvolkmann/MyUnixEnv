10e503a4e195fd85f3bbaa2cef9f2e5302ab4dc5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-09-30 11:05:17 -0500

    updated fish.html

--- .config/fish/functions/orderShirt.fish
+++ .config/fish/functions/orderShirt.fish
@@ -1,4 +1,5 @@
 function orderShirt -a size color
+  echo in orderShirt
   set -q size[1]; or set size 'large'
   set -q color[1]; or set color 'white'
   echo I see you want to order a $size shirt that is $color.
---
+++ .config/fish/functions/takeShelter.fish
@@ -0,0 +1,3 @@
+function takeShelter --on-event tornado --on-event ww3
+  echo Tornado warning! Take shelter!
+end
