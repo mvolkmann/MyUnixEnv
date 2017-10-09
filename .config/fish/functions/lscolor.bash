4842bcc7d76aee73d122aa4aee4df57c441a9451
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2017-10-08 19:58:01 -0500

    updated lscolor.bash

--- .config/fish/functions/lscolor.bash
+++ .config/fish/functions/lscolor.bash
@@ -1,12 +1,4 @@
 #!/bin/bash
-# This lists all files in the current directory, but files
-# with certain extensions are output with specific colors.
-# There are default colors for files with
-# the extensions .html, .css, .js, and .scss files.
-# These can be overridden and colors for other file extensions
-# can be specified with arguments of the form extension=color.
-# Files with unspecified extensions have a default color of white.
-# For example, lscolor fish=magenta js=cyan
 
 # Color ANSI escape sequences
 black='\033[0;30m'
@@ -18,6 +10,7 @@ purple='\033[0;35m' # magenta
 cyan='\033[0;36m'
 yellow='\033[1;33m'
 white='\033[1;37m'
+normal='\033[0m'
 
 # Default colors
 cssColor=$green
@@ -32,27 +25,25 @@ for arg in "$@"; do
   pieces=$arg
   if [ ${#pieces[@]} -lt 2 ]; then
     echo 'invalid argument "'$arg'"'
-    return 1
+    exit 1
   fi
   extension=${pieces[0]}
   color=${pieces[1]}
-  varName="${extension}Color}
+  varName="${extension}Color}"
   eval $varName=$color
 done
 
 for file in *; do
-  IFS="."
-  pieces=$file
-  if [ ${#pieces[@]} -ge 2 ]; then
-    extension=${pieces[1]}
-    varName="${extension}Color"
-    color=eval $$varName
+  extension=${file##*.}
+  # Two brackets is the only safe way!
+  if [[ $extension == $file ]]; then  # no extension was found
+    color=$defaultColor
+  else
+    colorName=${extension}Color
+    color=${!colorName}
     if [ -z $color ]; then
       color=$defaultColor
     fi
-  else
-    color=$defaultColor
   fi
-  #set_color $color
-  echo $file
+  echo -e "$color$file"
 done
