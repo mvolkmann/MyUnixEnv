511a7240b5573ccf1127d015c25242a16d7ae342
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-04-14 16:41:44 -0500

    more lazygit and astronvim config

--- .config/nvim/lua/user/plugins/community.lua
+++ .config/nvim/lua/user/plugins/community.lua
@@ -8,7 +8,19 @@ return {
     enabled = true
   },
   {
+    import = "astrocommunity.colorscheme.gruvbox",
+    enabled = true
+  },
+  {
     import = "astrocommunity.colorscheme.nightfox",
     enabled = true
   },
+  {
+    import = "astrocommunity.colorscheme.rose-pine",
+    enabled = true
+  },
+  {
+    import = "astrocommunity.colorscheme.tokyonight",
+    enabled = true
+  }
 }
