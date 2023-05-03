470871dffe023fd372b7a253b2a0c6826fd2c210
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-03 15:50:35 -0500

    more Neovim config

--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -1,6 +1,11 @@
 return {
   -- normal mode
   n = {
-    ["<leader>-"] = { "<cmd>split<cr>", desc = "Horizontal Split" }
+    ["<leader>-"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
+    ["<C-s>"] = { "<cmd>write<cr>", desc = "Write" }
+  },
+  i = {
+    -- TODO: Why doesn't this work?
+    ["<C-s>"] = { "jk<cmd>write<cr>a", desc = "Write" }
   }
 }
