fa7a9f9f6cc75d1fd751666431e9d37b9eb92e52
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-04 12:25:42 -0500

    added a key mapping

--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -2,7 +2,8 @@ return {
   -- normal mode
   n = {
     ["<leader>-"] = { "<cmd>split<cr>", desc = "Horizontal Split" },
-    ["<C-s>"] = { "<cmd>write<cr>", desc = "Write" }
+    ["<C-s>"] = { "<cmd>write<cr>", desc = "Write" },
+    ["sj"] = { "<cmd>TSJToggle<cr>", desc = "Toggle Split/Join" }
   },
   i = {
     -- TODO: Why doesn't this work?
