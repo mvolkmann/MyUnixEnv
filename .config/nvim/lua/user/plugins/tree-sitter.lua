9e9b3fea599a73cd351608a84678a4855e2e69e4
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-05 10:06:17 -0500

    latest

--- .config/nvim/lua/user/plugins/tree-sitter.lua
+++ .config/nvim/lua/user/plugins/tree-sitter.lua
@@ -1,4 +1,4 @@
-return {
+local treesitter = {
   "nvim-treesitter/nvim-treesitter",
   opts = function()
     require 'nvim-treesitter.configs'.setup {
@@ -24,3 +24,138 @@ return {
     }
   end
 }
+
+--[=[
+treesitter.config = function()
+  require('orgmode').setup_ts_grammar()
+
+  require('nvim-treesitter.configs').setup({
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { 'org' },
+    },
+    yati = {
+      enable = true,
+      default_lazy = true,
+      default_fallback = function(lnum, computed, bufnr)
+        return require('tmindent').get_indent(lnum, bufnr) + computed
+      end,
+    },
+    indent = {
+      enable = false,
+    },
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
+      persist_queries = false, -- Whether the query persists across vim sessions
+      keybindings = {
+        toggle_query_editor = 'o',
+        toggle_hl_groups = 'i',
+        toggle_injected_languages = 't',
+        toggle_anonymous_nodes = 'a',
+        toggle_language_display = 'I',
+        focus_language = 'f',
+        unfocus_language = 'F',
+        update = 'R',
+        goto_node = '<cr>',
+        show_help = '?',
+      },
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = 'gnn',
+        node_incremental = '.',
+        scope_incremental = 'gnc',
+        node_decremental = ',',
+      },
+    },
+    refactor = {
+      highlight_definitions = {
+        enable = true,
+      },
+      smart_rename = {
+        enable = true,
+        keymaps = {
+          smart_rename = 'gnr',
+        },
+      },
+      navigation = {
+        enable = true,
+        keymaps = {
+          goto_definition = 'gnd',
+          list_definitions = 'gnD',
+        },
+      },
+    },
+    autotag = {
+      enable = true,
+    },
+    textobjects = {
+      enable = true,
+      disable = {},
+      select = {
+        enable = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['aC'] = '@class.outer',
+          ['iC'] = '@class.inner',
+          ['ac'] = '@conditional.outer',
+          ['ic'] = '@conditional.inner',
+          ['ae'] = '@block.outer',
+          ['ie'] = '@block.inner',
+          ['al'] = '@loop.outer',
+          ['il'] = '@loop.inner',
+          ['is'] = '@statement.inner',
+          ['as'] = '@statement.outer',
+          ['ad'] = '@comment.outer',
+          ['am'] = '@call.outer',
+          ['im'] = '@call.inner',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+      move = {
+        enable = true,
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
+      lsp_interop = {
+        enable = true,
+        border = 'rounded',
+        peek_definition_code = {
+          ['<leader>dg'] = '@function.outer',
+          ['<leader>dG'] = '@class.outer',
+        },
+      },
+    },
+    ensure_installed = 'all',
+    ignore_install = { 'sql' },
+  })
+  return treesitter
+end
+]=]
+return treesitter
