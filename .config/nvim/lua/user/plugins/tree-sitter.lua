b5fd8681bc0717763ad26d81b0783d79c60240a7
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-05 09:14:41 -0500

    trying to configure nvim-orgmode

--- .config/nvim/lua/user/plugins/tree-sitter.lua
+++ .config/nvim/lua/user/plugins/tree-sitter.lua
@@ -4,6 +4,16 @@ return {
     require 'nvim-treesitter.configs'.setup {
       incremental_selection = {
         enable = true,
+
+        --[[ This is for nvim-orgmode and doesn't currently work.
+        ensure_installed = { 'org' },
+        highlight = {
+          enable = true,
+          -- Required for spellcheck, some LaTex highlights and
+          -- code block highlights that do not have ts grammar
+          additional_vim_regex_highlighting = { 'org' },
+        }, ]]
+
         keymaps = {
           init_selection = "<leader>sw",    -- select word
           node_incremental = "<leader>sn",  -- incremental select node
