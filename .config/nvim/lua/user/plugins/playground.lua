13e217c6fb5488fe19998bb96225edaba410453d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-08 14:39:42 -0500

    added new plugins

---
+++ .config/nvim/lua/user/plugins/playground.lua
@@ -0,0 +1,4 @@
+return {
+  "nvim-treesitter/playground",
+  lazy = false -- load on startup, not just when required
+}
---
+++ .config/nvim/lua/user/plugins/todo-quickfix.lua
@@ -0,0 +1,5 @@
+return {
+  "mvolkmann/todo-quickfix.nvim",
+  lazy = false, -- load on startup, not just when required
+  config = true -- require the plugin and call its setup function
+}
